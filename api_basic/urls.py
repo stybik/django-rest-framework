cd6ad23470f8d9071903165cb02b7c9aabeefa26
stybik <bikrocksbiswas@gmail.com>
2020-10-01 02:52:34 +0530

    Generic Views REST API

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
+from .views import *
 
 urlpatterns = [
     # path('article/', article_list),
     # path('detail/<int:pk>/', article_detail)
     path('article/', ArticleAPIView.as_view()),
     path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
 ]
